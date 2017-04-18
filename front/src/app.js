72979ca69bccd5cbe66f4311b9868db9cf5c00d4
kylejw1 <kylejw1@gmail.com>
2017-04-17 18:05:28 -0600

    Button grid with test data

--- front/src/app.js
+++ front/src/app.js
@@ -1,17 +1,35 @@
 
 var app = angular.module('vna', ['ngMaterial', 'ui.router', 'btford.socket-io']);
 
-app.config(function($stateProvider, $urlRouterProvider) {
+app.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
 
-  $stateProvider.state({
-    name: 'main',
-    url: '/',
-    templateUrl: 'view/main.html',
-    controller: 'MainController',
-    controllerAs: 'mainCtrl'
-  });
+  $stateProvider
+    
+    .state({
+      name: 'main',
+      url: '/',
+      templateUrl: 'view/main.html',
+      controller: 'MainController',
+      controllerAs: 'mainCtrl',
+      abstract: true
+    })
+    
+    .state({
+      name: 'main.order',
+      url: '^/order',
+      templateUrl: 'view/order.html',
+      controller: 'OrderController',
+      controllerAs: 'orderCtrl'
+    });
 
-  $urlRouterProvider.otherwise('/');
+  $urlRouterProvider.otherwise('/order');
+
+  $mdThemingProvider
+    .theme('default')
+    .primaryPalette('blue')
+    .accentPalette('teal')
+    .warnPalette('red')
+    .backgroundPalette('grey');
 
 });
 
---
+++ front/src/order.controller.js
@@ -0,0 +1,40 @@
+app.controller('OrderController', ['vnaSocket', function(vnaSocket) {
+
+  var vm = this;
+
+  vm.types = [
+    { name: "Pepperoni" },
+    { name: "Hawaiian" },
+    { name: "Meat Lovers" },
+    { name: "Vegetarian" },
+    { name: "Cheese" },
+    { name: "BBQ Chicken" },
+    { name: "Deluxe" },
+    { name: "Bacon" },
+    { name: "Pepperoni" },
+    { name: "Hawaiian" },
+    { name: "Cheese" },
+    { name: "Vegetarian" },
+    { name: "Deluxe" },
+    { name: "BBQ Chicken" },
+    { name: "Bacon" },
+    { name: "Meat Lovers" },
+    { name: "Pepperoni" },
+    { name: "Bacon" },
+    { name: "Pepperoni" },
+    { name: "Hawaiian" },
+    { name: "Cheese" },
+    { name: "Vegetarian" },
+    { name: "Hawaiian" },
+    { name: "Meat Lovers" },
+    { name: "Vegetarian" },
+    { name: "Cheese" },
+    { name: "BBQ Chicken" },
+    { name: "Deluxe" },
+    { name: "Deluxe" },
+    { name: "BBQ Chicken" },
+    { name: "Bacon" },
+    { name: "Meat Lovers" }
+  ]
+
+}]);
