3c888043ce2069d7177b6a53dcfc081efec437f0
Kyle W <kylejw1>
2017-04-23 22:57:48 -0600

    Number spinner

--- front/src/status/edit.status.controller.js
+++ front/src/status/edit.status.controller.js
@@ -3,6 +3,9 @@ app.controller('EditStatusController', [ '$mdDialog', 'order',
 
   var vm = this;
 
+  vm.minutes = 0;
+  vm.seconds = 0;
+
   vm.order = order;
 
   vm.hide = function() {
@@ -16,6 +19,34 @@ app.controller('EditStatusController', [ '$mdDialog', 'order',
   vm.answer = function(answer) {
     $mdDialog.hide(answer);
   };
+
+  vm.incrementMinutes = function() {
+    vm.minutes = vm.setMinutes(vm.minutes + 1);
+  }
+
+  vm.decrementMinutes = function() {
+    vm.minutes = vm.setMinutes(vm.minutes - 1);
+  }
+
+  vm.incrementSeconds = function() {
+    vm.seconds = vm.setSeconds(vm.seconds + 1);
+  }
+
+  vm.decrementSeconds = function() {
+    vm.seconds = vm.setSeconds(vm.seconds - 1);
+  }
+
+  vm.setMinutes = function(value) {
+    return constrain(value, 0, 99);
+  }
+
+  vm.setSeconds = function(value) {
+    return constrain(value, 0, 59);
+  }
+
+  function constrain(value, min, max) {
+    return Math.max(Math.min(value, max), min);
+  }
 
 }]);
 
